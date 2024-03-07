43b6cf1458389745ec96076771bf2c6e72547bb7
EdKryp <edwincano653@gmail.com>
2024-03-07 02:32:20 -0600

    n

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {View, Text, TextInput, Button, StyleSheet, Alert} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
 
 export default App = () => {
   const [expediente, setExpediente] = useState('');
@@ -7,43 +7,51 @@ export default App = () => {
 
   const handleSubmit = () => {
     if (expediente !== '' && password !== '') {
-      Alert.alert('Usuario autenticado');
+      if (password.length >= 8) {
+        Alert.alert('Authenticated User');
+      } else {
+        Alert.alert('Password must be at least 8 characters');
+      }
     } else {
-      Alert.alert('Usuario no autenticado');
+      Alert.alert('Unauthenticated User');
     }
   };
 
   return (
     <View style={styles.container}>
+
       <Text style={styles.title}>Login</Text>
+
       <View style={styles.inputContainer}>
         <TextInput
-          style={styles.textInput} 
-          placeholder="Expediente"
+          style={styles.TextInput}
+          placeholder="File Number"
           maxLength={6}
           keyboardType="numeric"
-          onChangeText={expediente => {
-            setExpediente(expediente);
+          onChangeText={exp => {
+            setExpediente(exp);
           }}
           value={expediente}
         />
-        <TextInput 
-          style={styles.textInput}
-          placeholder="Contraseña"
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password"
           maxLength={10}
           secureTextEntry={true}
-          onChangeText={(password) => {
-            setPassword(password);
+          onChangeText={pass => {
+            setPassword(pass);
           }}
           value={password}
         />
-      </View> 
+      </View>
       <View style={styles.buttonContainer}>
         <Button
-          title="Iniciar sesión" 
+          buttonStyle={[styles.button, { backgroundColor: '#154854', fontSize: 18 }]}
+          title="Sign in"
           onPress={handleSubmit}
         />
       </View>
+
     </View>
   );
 }
@@ -53,37 +61,35 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'white',
+    backgroundColor: '#03989E', 
   },
   title: {
-    fontSize: 30,
+    fontSize: 36, 
     fontWeight: 'bold',
+    color: 'white', 
     marginBottom: 20,
   },
   inputContainer: {
     width: '80%',
-    backgroundColor: 'white',
-    borderRadius: 10,
-    padding: 20,
-    shadowColor: 'black',
-    shadowOpacity: 10,
-    shadowRadius: 5,
-    elevation: 5,
-  },
-  textInput: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
+    marginVertical: 10,
     borderRadius: 5,
-    marginBottom: 10,
-    paddingLeft: 10,
+    margin: 10,
+    padding: 10,
+    backgroundColor: '#31849B', 
   },
   buttonContainer: {
-    width: '60%',
-    marginTop: 20,
+    width: '80%',
+    marginVertical: 20,
+  },
+  button: {
+    color: 'white', 
+  },
+  TextInput: {
+    fontSize: 16, 
+    color: 'black',
+    backgroundColor: 'white',
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 5,
   },
 });
-
-
-
-
